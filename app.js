9b8c55cdf286c3d0cbc34b473d8c59dcccde5201
abachant <aubreybachant@gmail.com>
2021-03-19 15:27:10 -0400

    Update /products endpoint to hand page and count body parameters

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const client = require('./database');
 const app = express();
 const port = 3000;
@@ -7,10 +8,29 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Atelier Products API');
 });
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+
 // Retrieves the list of products.
 // GET /products
 app.get('/products', (req, res) => {
-  client.query('SELECT * FROM products')
+  let page = 1;
+  let count = 5;
+  // update page and count values if client sent params for page or count
+  if(req.body.page) {
+    page = parseInt(req.body.page);
+  };
+  if(req.body.count) {
+    count = parseInt(req.body.count);
+  };
+
+  // SELECT * FROM reviews WHERE product_id = ${product_id} LIMIT ${response.count} OFFSET ${response.page * response.count}
+
+  const queryText = `SELECT * FROM products LIMIT ${count} OFFSET ${(page * count) - count}`;
+  client.query(queryText)
   .then(dbRes => res.send(dbRes.rows))
   .catch(e => console.error(e))
 });
@@ -18,6 +38,7 @@ app.get('/products', (req, res) => {
 // Returns all product level information for a specified product id.
 // GET /products/:product_id
 app.get('/products/:product_id', (req, res) => {
+  console.log(req.body)
   res.send(req.params.product_id)
 });
 
